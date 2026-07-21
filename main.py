58fd7c208d33a187408e326b07c331bf4d873166
Mohib ul Haq <muhibhaq123@gmail.com>
2026-07-21 15:12:10 +0500

    feat: implement basic terminal-based web scraping and OpenAI-based article summarization pipeline

--- main.py
+++ main.py
@@ -1,6 +1,54 @@
-def main():
-    print("Hello from article-summarizer!")
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+from trafilatura import fetch_url, extract
 
+# Load environment variables from .env file
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+client = OpenAI(
+    api_key=os.environ.get("OPEN_ROUTER_KEY"),
+    base_url=os.environ.get("MODEL_BASE_URL")
+)
+
+downloaded = fetch_url("https://en.wikipedia.org/wiki/Artificial_intelligence")
+result = extract(downloaded)
+
+response = client.chat.completions.create(
+    model=os.environ.get("MODEL_NAME"),
+    messages=[
+        {"role": "system", "content": """Summarize the article.
+Return:
+
+- A concise title.
+- Between 6 and 10 key points.
+- Each key point should be one complete sentence.
+- Do not create separate heading bullets.
+- Do not output incomplete phrases.
+- Each bullet should summarize one major idea.
+- End with a one-sentence TL;DR."""},
+        {"role": "user", "content": f"Summarize the following text:\n\n{result}"}
+    ],
+    response_format={
+        "type": "json_schema",
+        "json_schema": {
+            "name": "summary",
+            "strict": True,
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "title": { "type": "string" },
+                    "key_points": { 
+                        "type": "array",
+                        "items": {
+                            "type": "string"
+                        } 
+                    },
+                    "one_line TL;DR": { "type": "string" }
+                }
+            }
+        }
+    }
+)
+
+print(response.choices[0].message.content)
